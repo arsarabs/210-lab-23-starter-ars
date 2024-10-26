f13a573716ab2ccc5969dd5b01829e9660dccfa7
Your Name <your-email@example.com>
2024-10-26 04:28:20 -0700

    rearranging starter main for simplicity

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
+// COMSC-210 | Lab 23 | Anthony R. Sarabia
+// IDE used: Visual Studio
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <list>
 #include "Goat.h"
 using namespace std;
-     //testing
+    
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
 int select_goat(list<Goat> trip);
@@ -13,25 +16,33 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
+
+
 int main() {
+
+    //Variables all in one place
     srand(time(0));
-    bool again;
+    bool again = true; // initliaze to something
+    string names[SZ_NAMES];
+    string colors[SZ_COLORS];
+    int i = 0;
+    ifstream fin("names.txt");
+    ifstream fin1("colors.txt");
 
     // read & populate arrays for names and colors
-    ifstream fin("names.txt");
-    string names[SZ_NAMES];
+  
+    
+
     int i = 0;
     while (fin >> names[i++]);
     fin.close();
-    ifstream fin1("colors.txt");
-    string colors[SZ_COLORS];
+
     i = 0;
     while (fin1 >> colors[i++]);
     fin1.close();
 
-
-
-
     return 0;
 }
+
+
 
